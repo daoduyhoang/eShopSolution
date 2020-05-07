09eeb2ea31afa6d2fe6239ef71b599b78f42f677
name <you@example.com>
2020-05-07 22:25:59 +0700

    Add project files.

---
+++ eShop.WebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace eShop.WebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
